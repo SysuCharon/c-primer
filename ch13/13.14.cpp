b30a3aa1e71c5379157d33c8301e543fa174e2e2
charon <lu_charon@qq.com>
2019-10-21 08:59:44 +0800

    sec 13.1.4

---
+++ ch13/13.14.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Num {
+public:
+    int num;
+    Num(): num(0) {}
+    Num(const Num& n) {
+        num = n.num + 1;
+    }
+};
+
+void f1(Num n) {
+    cout << n.num << endl;
+}
+
+void f2(const Num& n) {
+    cout << n.num << endl;
+}
+
+int main() {
+    Num a, b = a, c = b;
+    f1(a);
+    f1(b);
+    f1(c);
+
+    f2(a);
+    f2(b);
+    f2(c);
+}

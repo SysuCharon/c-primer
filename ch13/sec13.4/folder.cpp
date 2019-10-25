c81372a0d880c4e42c514f2bd9172ef7aa55e31b
charon <lu_charon@qq.com>
2019-10-25 09:16:05 +0800

    sec13.4

--- ch13/sec13.4/folder.cpp
+++ ch13/sec13.4/folder.cpp
@@ -2,9 +2,55 @@
 
 using namespace std;
 
+Folder::addMsg(const Message* msg) {
+    _messages.insert(msg);
+}
+
+Folder::remMsg(const Message* msg) {
+    if (_messages.find(msg) != _messages.end()) {
+        _message.erase(msg);
+    }
+}
+
 Message::Message(const Message& rhs)
     : _contents(rhs._contents), _floders(rhs._folders) {
     for (auto f: _floders) {
-        addToFloders(f);
+        addToFolders(rhs);
     }
+}
+
+void Message::save(Folder& f) {
+    _floders.insert(&f);
+    f.addMsg(this);
+}
+
+void Message::remove(Folder& f) {
+    if (_floders.find(&f) != _floders.end()) {
+        _floders.erase(&f);
+        f.remMsg(this);
+    }
+}
+
+void Message::addToFolders(const Message& msg) {
+    for (auto f: msg._folders) {
+        f->addMsg(this);
+    }
+}
+
+void Message::removeFromFolders() {
+    for (auto f: msg._folders) {
+        f->remMsg(this);
+    }
+}
+
+Message::~Message() {
+    removeFromFolders();
+}
+
+Message& Message::operator=(const Message& rhs) {
+    removeFromFolders();
+    _contents = rhs._contents;
+    _floders = rhs._floders;
+    addToFolders(rhs);
+    return *this;
 }
--- ch13/sec13.4/folder.h
+++ ch13/sec13.4/folder.h
@@ -25,7 +25,7 @@ private:
     void removeFromFolders();
 
     std::string _contents;
-    std::unordered_set<std::shared_ptr<Folder>> _floders;
+    std::unordered_set<Folder*> _floders;
 }
 
 class Folder {
@@ -35,7 +35,7 @@ public:
     remMsg(const Message& msg);
 
 private:
-    std::unordered_set<std::shared_ptr<Message>> _messages;
+    std::unordered_set<Message*> _messages;
 }
 
 #endif
